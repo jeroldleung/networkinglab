9f9a690660ce54efeffc91b78047118af1d997cc
trabemazusa <zihao.liang@qq.com>
2023-08-29 17:27:57 +0800

    bytestream basics

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -4,34 +4,37 @@
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ), buffer_(), nbytes_( 0 ), closed_( false ), error_( false ) {}
 
 void Writer::push( string data )
 {
   // Your code here.
-  (void)data;
+  buffer_.push( data );
+  nbytes_ += data.size();
 }
 
 void Writer::close()
 {
   // Your code here.
+  closed_ = true;
 }
 
 void Writer::set_error()
 {
   // Your code here.
+  error_ = true;
 }
 
 bool Writer::is_closed() const
 {
   // Your code here.
-  return {};
+  return { closed_ };
 }
 
 uint64_t Writer::available_capacity() const
 {
   // Your code here.
-  return {};
+  return { capacity_ - nbytes_ };
 }
 
 uint64_t Writer::bytes_pushed() const
@@ -49,13 +52,13 @@ string_view Reader::peek() const
 bool Reader::is_finished() const
 {
   // Your code here.
-  return {};
+  return { closed_ && ( nbytes_ == 0 ) };
 }
 
 bool Reader::has_error() const
 {
   // Your code here.
-  return {};
+  return { error_ };
 }
 
 void Reader::pop( uint64_t len )
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -13,6 +13,11 @@ class ByteStream
 protected:
   uint64_t capacity_;
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
+  std::queue<std::string> buffer_;
+  uint64_t nbytes_;
+
+  bool closed_;
+  bool error_;
 
 public:
   explicit ByteStream( uint64_t capacity );
