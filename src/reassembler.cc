9f467ccc2d6074e4ea193fd858ecd0067d76a39c
jeroldleung <jeroldleung@gmail.com>
2023-09-05 22:53:40 +0800

    optimize the Reassembler overlap cases handling

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -18,12 +18,15 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     output.push( std::move( data.erase( 0, output.bytes_pushed() - first_index ) ) );
   }
 
+  // prevent out of range
+  data.resize( min( data.size(), output.available_capacity() - first_index ) );
+
   // edge cases
-  if ( !data.empty() && ( substrs_.empty() || first_index <= substrs_.front().starti_ ) ) {
+  if ( !data.empty() && substrs_.empty() ) {
     nbytes_substrs_ += data.size();
     substrs_.emplace_front( first_index, std::move( data ) );
   }
-  if ( !data.empty() && first_index >= substrs_.back().starti_ ) {
+  if ( !data.empty() && first_index > substrs_.back().starti_ ) {
     nbytes_substrs_ += data.size();
     substrs_.emplace_back( first_index, std::move( data ) );
   }
@@ -31,24 +34,23 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   for ( auto iter = substrs_.begin(); iter != substrs_.end(); ) {
     // insert to the substrs_ until earlier bytes become known
     if ( !data.empty() && first_index <= iter->starti_ ) {
-      iter = substrs_.insert( iter, { first_index, std::move( data ) } );
       nbytes_substrs_ += data.size();
+      iter = substrs_.insert( iter, { first_index, std::move( data ) } );
     }
     // overlap with the previous one
     auto prev = std::prev( iter );
+    // some part overlapped
     if ( prev != substrs_.end() && iter->starti_ >= prev->starti_
-         && iter->starti_ + iter->data_.size() <= prev->starti_ + prev->data_.size() ) {
-      nbytes_substrs_ -= iter->data_.size();
-      iter = substrs_.erase( iter );
-      continue;
-    }
-    // overlap with the next one
-    auto next = std::next( iter );
-    if ( next != substrs_.end() && iter->starti_ >= next->starti_
-         && iter->starti_ + iter->data_.size() <= next->starti_ + next->data_.size() ) {
-      nbytes_substrs_ -= iter->data_.size();
-      iter = substrs_.erase( next );
-      continue;
+         && iter->starti_ < prev->starti_ + prev->data_.size() ) {
+      const int nbytes_overlapped = min( prev->starti_ + prev->data_.size() - iter->starti_, iter->data_.size() );
+      nbytes_substrs_ -= nbytes_overlapped;
+      iter->data_.erase( 0, nbytes_overlapped );
+      iter->starti_ += nbytes_overlapped;
+      // fully overlapped
+      if ( iter->data_.empty() ) {
+        iter = substrs_.erase( iter );
+        continue;
+      }
     }
     // no overlap
     ++iter;
