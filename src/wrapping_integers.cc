1b72cf5e8c81e00fcef415cb9f5b0c6378997dd7
jeroldleung <jeroldleung@gmail.com>
2023-09-07 11:08:42 +0800

    complete wrapping integers

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,23 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + ( n & UINT32_MAX );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint64_t absolute_seqno
+    = ( static_cast<uint64_t>( raw_value_ ) + ( 1UL << 32 ) - zero_point.raw_value_ ) % ( 1UL << 32 );
+
+  absolute_seqno += ( checkpoint >> 32 ) * ( 1UL << 32 );
+
+  if ( absolute_seqno < checkpoint ) {
+    absolute_seqno += ( 1UL << 32 );
+  }
+
+  if ( checkpoint != 0 && absolute_seqno - checkpoint > ( 1UL << 31 ) ) {
+    absolute_seqno -= ( 1UL << 32 );
+  }
+
+  return absolute_seqno;
 }
